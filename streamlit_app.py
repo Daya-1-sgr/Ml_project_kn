10c85ead2c4690bfd5045f1b69775014d9c75624
Daya-1-sgr <dayabansgr@gmail.com>
2024-11-04 10:05:55 -0500

    third streamlit check

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,10 @@ parental_level_of_education=st.selectbox('Parental_level_of_education',("bachelo
 lunch=st.selectbox('lunch',('standard', 'free/reduced'))
 test_preparation_course=('test_preparation_course',('none', 'completed'))
 reading_score=st.slider('reading score',10,100,68)
-writing_score=st.sliter('writing_score',17,100,69)
+writing_score=st.slider('writing_score',17,100,69)
 
 data={'gender':gender,'race_ethnicity':race_ethnicity,'parental_level_of_education':parental_level_of_education
       ,'lunch':lunch,'test_preparation_course':test_preparation_course,'reading_score':reading_score,'writing_score':writing_score}
 input_dataframe=pd.DataFrame(data)
 
+input_dataframe
