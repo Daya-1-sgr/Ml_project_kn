4a65676e86ae482b8a6fe0816d1c864d6d3776a8
Daya-1-sgr <dayabansgr@gmail.com>
2024-11-04 09:35:10 -0500

    streamlit_trial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from src.pipeline.predict_pipeline import PredictPipeline,CustomData
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
 
 st.title('Hello there')
+
+st.header('input features')
+gender=st.selectbox('gender',('male','female'))
+#race_ethnicity=st.selectbox('race_ethnicity',(''))
