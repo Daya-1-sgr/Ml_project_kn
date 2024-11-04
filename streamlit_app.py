6342a4afd229ff7857088193c09946cf50229c19
Daya-1-sgr <dayabansgr@gmail.com>
2024-11-04 13:00:10 -0500

    streamlit 3rd commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,15 @@ test_preparation_course=st.selectbox('test_preparation_course',('none', 'complet
 reading_score=st.slider('reading score',10,100,68)
 writing_score=st.slider('writing_score',17,100,69)
 
+
 data={'gender':gender,'race_ethnicity':race_ethnicity,'parental_level_of_education':parental_level_of_education
       ,'lunch':lunch,'test_preparation_course':test_preparation_course,'reading_score':reading_score,'writing_score':writing_score}
-input_dataframe=pd.DataFrame(data,index=[0])
+input_dataframe=CustomData.get_data_as_frame(data)
 
 input_dataframe
+
+prediction=PredictPipeline.predict(input_dataframe)
+st.title('the prediction is :')
+prediction
+
+
