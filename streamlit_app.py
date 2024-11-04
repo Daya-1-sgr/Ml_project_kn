6cb0d826a4035fa40fedcfbf9dda6ba1630f9d97
Daya-1-sgr <dayabansgr@gmail.com>
2024-11-04 10:10:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ race_ethnicity=st.selectbox('race_ethnicity',('group B', 'group C', 'group A', '
 parental_level_of_education=st.selectbox('Parental_level_of_education',("bachelor's degree", 'some college', "master's degree",
        "associate's degree", 'high school', 'some high school'))
 lunch=st.selectbox('lunch',('standard', 'free/reduced'))
-test_preparation_course=('test_preparation_course',('none', 'completed'))
+test_preparation_course=st.selectbox('test_preparation_course',('none', 'completed'))
 reading_score=st.slider('reading score',10,100,68)
 writing_score=st.slider('writing_score',17,100,69)
 
